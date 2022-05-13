39bed73e3f5f2bbb7b05c4c6434dec07036db760
user <user@users-MacBook-Pro.local>
2022-05-13 11:21:23 +0100

    Added more needed routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 // /admin/add-product => GET
 router.get("/add-product", productsController.getAddProducts);
 
 // /admin/add-product => POST
 router.post("/add-product", productsController.postAddProducts);
+
+// /admin/edit-products => GET
+router.get("/edit-product", adminController.getEditProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,16 @@ const router = express.Router();
 
 const productsController = require("../controllers/products");
 
-router.get("/", productsController.getProducts);
+router.get("/", productsController.getIndex);
+
+router.get("/products-list", productsController.getProductsList);
+
+router.get("/cart", productsController.getCart);
+
+router.get("/checkout", productsController.getCheckout);
+
+router.get("/product-details", productsController.getProductDetails);
+
+router.get("/products-list", productsController.getProductsList);
 
 module.exports = router;
