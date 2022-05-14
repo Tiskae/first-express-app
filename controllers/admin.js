2204090cc32465722f8b0221b058a13bceb89eb5
user <user@users-MacBook-Pro.local>
2022-05-14 15:47:35 +0100

    Futher app buildup

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const Product = require("../models/product");
+
 exports.getEditProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     docTitle: "Edit Product",
@@ -6,8 +8,12 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("admin/products", {
-    docTitle: "Admin Products",
-    path: "/admin/products",
+  Product.fetchAll((products) => {
+    console.log(products);
+    res.render("admin/products", {
+      docTitle: "Admin Products",
+      path: "/admin/products",
+      prods: products,
+    });
   });
 };
