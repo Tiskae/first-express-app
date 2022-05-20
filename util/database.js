dd82381ebd0cbd54eba3a23360c180c600b61538
user <user@users-MacBook-Pro.local>
2022-05-20 11:22:16 +0100

    sequelize init

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node_app", "root", 75865335, {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
