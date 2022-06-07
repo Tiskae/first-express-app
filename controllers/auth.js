200cd4cf587ef47c3dc136de4c8b671452eddc81
user <user@users-MacBook-Pro.local>
2022-06-07 12:34:55 +0100

    Init email sending

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,43 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+// const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "tiskae200@gmail.com",
+    pass: "macbook100$",
+  },
+});
+
+const mailOptions = {
+  from: "tiskae200@gmail.com",
+  to: "adedokuntobiloba100@gmail.com",
+  subject: "From node app",
+  text: "That was quite easy",
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) console.log(error);
+  else console.log("Email sent, " + info.response);
+});
 
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
   // const isAuthenticated = req.get("Cookie").split("=")[1] === "true";
 
-  console.log(req.session);
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
 
   res.render("auth/login", {
     docTitle: "Login",
     path: "/login",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -19,12 +46,14 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
+        req.flash("error", "Invalid email or password");
         return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
         .then(doMatch => {
           if (!doMatch) {
+            req.flash("error", "Invalid email or password");
             return res.redirect("/login");
           }
           req.session.isLoggedIn = true;
@@ -42,10 +71,17 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render("auth/signup", {
     docTitle: "Sign up",
     path: "/signup",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -57,6 +93,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash("error", "Email is taken already!");
         return res.redirect("/signup");
       }
       return bcrypt
